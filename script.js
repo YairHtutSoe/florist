9432314ceca7d91279ec333dd947ad3b0c2ea291
YeHtut <ys4ndr0@gmail.com>
2022-01-09 22:21:09 +0630

    jan 9 2022

---
+++ script.js
@@ -0,0 +1,64 @@
+let btn1 = document.getElementById('all');
+let btn2 = document.getElementById('bouquet');
+let btn3 = document.getElementById('box');
+let btn4 = document.getElementById('shelf');
+let btn5 = document.getElementById('basket');
+let btn6 = document.getElementById('gift');
+
+
+btn1.onclick = function abc(){
+	gallery1.style.display = 'grid';
+	gallery2.style.display = 'none';
+	gallery3.style.display = 'none';
+	gallery4.style.display = 'none';
+	gallery5.style.display = 'none';
+	gallery6.style.display = 'none';
+};
+btn2.onclick = function abc(){
+	gallery1.style.display = 'none';
+	gallery2.style.display = 'grid';
+	gallery3.style.display = 'none';
+	gallery4.style.display = 'none';
+	gallery5.style.display = 'none';
+	gallery6.style.display = 'none';
+};
+btn3.onclick = function abc(){
+	gallery1.style.display = 'none';
+	gallery2.style.display = 'none';
+	gallery3.style.display = 'grid';
+	gallery4.style.display = 'none';
+	gallery5.style.display = 'none';
+	gallery6.style.display = 'none';
+};
+btn4.onclick = function abc(){
+	gallery1.style.display = 'none';
+	gallery2.style.display = 'none';
+	gallery3.style.display = 'none';
+	gallery4.style.display = 'grid';
+	gallery5.style.display = 'none';
+	gallery6.style.display = 'none';
+};
+btn5.onclick = function abc(){
+	gallery1.style.display = 'none';
+	gallery2.style.display = 'none';
+	gallery3.style.display = 'none';
+	gallery4.style.display = 'none';
+	gallery5.style.display = 'grid';
+	gallery6.style.display = 'none';
+};
+btn6.onclick = function abc(){
+	gallery1.style.display = 'none';
+	gallery2.style.display = 'none';
+	gallery3.style.display = 'none';
+	gallery4.style.display = 'none';
+	gallery5.style.display = 'none';
+	gallery6.style.display = 'grid';
+};
+
+
+
+
+
+
+
+
